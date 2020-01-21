73769918ceecd730b1a7df5fed73d4bb98efcdca
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-01-21 16:20:30 -0500

    Complete lab

--- GC_Lab_2.1/Program.cs
+++ GC_Lab_2.1/Program.cs
@@ -6,7 +6,70 @@ namespace GC_Lab_2._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome Human, allow me to calculate your rooms for you. >:-)");
+
+            while (true)
+            {
+                Console.Write("Enter a room length: ");
+                Double.TryParse(Console.ReadLine(), out double length);
+                Console.Write("Enter a room width: ");
+                Double.TryParse(Console.ReadLine(), out double width);
+                Console.Write("Enter a room height: ");
+                Double.TryParse(Console.ReadLine(), out double height);
+
+                var area = length * width;
+                var perimeter = (2 * length) + (2 * width);
+                var volume = area * height;
+
+                string roomSize;
+
+                if (area <= 250)
+                {
+                    roomSize = "small.";
+                }
+                else if (area < 650)
+                {
+                    roomSize = "medium!";
+                }
+                else if (area >= 650)
+                {
+                    roomSize = "large!!";
+                }
+                else
+                {
+                    roomSize = "undefined!!!";
+                }
+
+                Console.WriteLine($"This room's perimeter is {perimeter} feet long.");
+                Console.WriteLine($"This room's area is {area} square feet.");
+                Console.WriteLine($"This room's volume is: {volume} cubed feet.");
+                Console.WriteLine($"This room is {roomSize}");
+
+                Console.Write("Give us more rooms to... measure? (y/n) ");
+            getYesNo:
+                ConsoleKeyInfo answer = Console.ReadKey();
+                Console.WriteLine();
+
+                if (answer.KeyChar == 'y')
+                {
+                    continue;
+                }
+                else if (answer.KeyChar == 'n')
+                {
+                    break;
+                }
+                else
+                {
+                    goto getYesNo;
+                }
+
+
+            }
+
+            Console.WriteLine("Thank you, Human, we will make use of this detailed room informaiton.");
+
+
         }
+
     }
 }
